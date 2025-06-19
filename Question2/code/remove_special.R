5318bba06cbce630912e71a0710782bb68af3095
charisageyer22894551 <charisageyer@gmail.com>
2025-06-19 06:00:53 +0200

    updates updates

--- Question2/code/remove_special.R
+++ Question2/code/remove_special.R
@@ -1,9 +1,10 @@
 
-# remove_special <- function(df, col_name) {
-#     df %>%
-#         mutate(across({{col_name}}, iconv, from = "UTF-8", to = "ASCII", sub = ""))
-# }
+remove_special <- function(df, col_name) {
+    df %>%
+        mutate(across({{col_name}}, iconv, from = "UTF-8", to = "ASCII", sub = ""))
+}
 
+#
 # remove_special <- function(df, col_name) {
 #     df %>%
 #         mutate(col_name = iconv({{col_name}}, from = "UTF-8", to = "ASCII", sub = ""))
@@ -11,12 +12,12 @@
 
 # doesnt pick up col_name! need to specify with {{}}. Easeir to just use across
 
+# #
+# remove_special <- function(df, col) {
+#   df %>%
 #
-remove_special <- function(df, ...) {
-  df %>%
-
-                 mutate(across(c(...), ~iconv(., from = "UTF-8", to = "ASCII", sub = "")))
-}
+#                  mutate(across(c(...), ~iconv(., from = "UTF-8", to = "ASCII", sub = "")))
+# }
 # } #that gives me error now :( )
 
 # even nicer with purr :) now I can use it for many cols at a time
